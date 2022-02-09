99a4e113f41086059c4ebf2285f23fc35bb5e041
Dhanya <60929233+DHANYAV20@users.noreply.github.com>
2022-02-10 01:28:19 +0530

    Cfn Intrinsic Function

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -1,19 +1,12 @@
 import aws_cdk.aws_s3 as _s3
 from aws_cdk.core import Stack
 from constructs import Construct
-
+import aws_cdk.core as core
 class CdkAppStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
         _s3.Bucket(
             self,
             "myBucketId",
@@ -22,3 +15,24 @@ class CdkAppStack(Stack):
             encryption=_s3.BucketEncryption.S3_MANAGED,
             block_public_access = _s3.BlockPublicAccess.BLOCK_ALL
         )
+        mybucket =  _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+        snstopicname = "abczys1234"
+
+        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Maximum value can be only 18 characters")
+
+        print(mybucket.bucket_name)
+
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My first CDK Bucket",
+            export_name="myBucketOutput1"
+        )
+
+
